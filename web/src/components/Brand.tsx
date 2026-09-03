2a84676527d78536e7a8bedc2b144876209f7f64
Dustin Pilgrim <dustin.pilgrim1997@gmail.com>
2026-09-03 02:05:01 -0300

    refine(web): enlarge rail brand mark

--- web/src/components/Brand.tsx
+++ web/src/components/Brand.tsx
@@ -8,7 +8,7 @@ interface BrandProps {
 export function Brand(props: BrandProps) {
   return (
     <div class="brand" classList={{ compact: props.compact }} aria-label="Hames">
-      <Icon name="brand.mark" class="brand-mark" size={props.compact ? 31 : 22} />
+      <Icon name="brand.mark" class="brand-mark" size={props.compact ? 36 : 22} />
       <Show when={!props.compact}>
         <span class="brand-name">Hames</span>
         <span class="brand-surface">Web</span>
